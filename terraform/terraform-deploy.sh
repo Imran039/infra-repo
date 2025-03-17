9dcd4357fe846cefa5587aee2b6d7f2dd106ce6a
Imran039 <science332@gmail.com>
2025-03-17 20:30:18 -0300

    Fixing path

--- terraform/terraform-deploy.sh
+++ terraform/terraform-deploy.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
-set -e  # Exit on error
+set -e  # Exit immediately if a command exits with a non-zero status
+
+# Navigate to the Terraform directory
+cd terraform
 
 # Initialize Terraform
 terraform init
